e549081b1540ca708622055c1bbcb3764a5c61ad
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-06-13 18:13:50 -0700

    Made all changes to make this game works! Now it wokrs completely. I did made reset function and other functions associated with the reset function. Also, added the footer to hide the space.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,45 @@
+
 $(function() {
     var wins = 0;
     var loses = 0;
     var currentScore = 0;
     var targetScore = 0;
+    var htmlTopLeftHead = $('#top-left-head').html();
+    var htmlTopMiddleHead = $('#top-middle-head').html();
 
-    var targetScore = function() {
+
+
+    var targetSc = function() {
         targetScore = Math.floor(Math.random()*140) - 20;
         while(targetScore < 19) {
             targetScore = Math.floor(Math.random()*140) - 20;
         }
-        return targetScore;
     }
+    
+    targetSc();
 
-    $('#targetScore').html(targetScore());
-
+    $('#targetScore').html(targetScore);
     var randomNumberGenerator = function () {
         var randomNum = Math.floor(Math.random()*11) + 1;
         return randomNum;
 
     }
+    // class Letter {
+    //     constructor(letter, value) {
+    //         this.letter = letter;
+    //         this.value = value;
+    //     }
+    //     getLetter = function() {
 
-    
+    //     }
+    // }
 
     var Letter = function(letter, value) {
-        this.letter = letter,
-        this.value = value,
-        this.getLetter = function() {return this.letter;},
+        this.letter = letter;
+        this.value = value;
+        this.getLetter = function() {return this.letter;}
         this.getValue = function() {return this.value;}
+        this.resetValue = function() {this.value = randomNumberGenerator();}
     }
     
     var m = new Letter('M', randomNumberGenerator());
@@ -35,9 +48,22 @@ $(function() {
     var v = new Letter('V', randomNumberGenerator());
     var e = new Letter('E', randomNumberGenerator());
     var l = new Letter('L', randomNumberGenerator());
-    
     var marvel = new Array (m, a, r, v, e, l);
 
+    var reset = function() {
+        targetSc();
+        resetNum();
+        currentScore = 0;
+    };
+
+    var resetNum = function() {
+        m.resetValue();
+        a.resetValue();
+        r.resetValue();
+        v.resetValue();
+        e.resetValue();
+        l.resetValue();
+    }
 
     $('.letters').on('click', function() {
 
@@ -61,27 +87,50 @@ $(function() {
     if (currentScore > targetScore) {
         $('#headInstruction').empty();
         $('#headInstruction').html('YOU LOSE!!')
-       
+        // $('#top-left-head').html("<button id=\"top-left-button\">Restart</button>")
+        // $('#top-left-button').on('click', function reset() {
+        //     $('#top-left-head').html(htmlTopLeftHead);
+        //     $('#top-middle-head').html(htmlTopMiddleHead);
+        // })
+        afterFinish();
         loses++;
+        reset();
+        updateResults();
     } else if (currentScore === targetScore) {
         $('#headInstruction').empty();
         $('#headInstruction').html('YOU WON!!')
+        afterFinish();
         wins++;
-   
+        reset();
+        updateResults();
     }
  
-
- 
     updateResults();
+ 
+   
 
     });
+
+    var afterFinish = function() {
+        $('#top-left-head').html("<p id=\"top-left-button\" class=\"middle-head\">Restart</p>")
+        $('#top-left-button').on('click', function resetOne() {
+            $('#top-left-head').html(htmlTopLeftHead);
+            $('#top-middle-head').html(htmlTopMiddleHead);
+            $('#targetScore').html(targetScore);
+        })
+    }
+ 
 
     var updateResults = function() {
 
     $('#wins').html(wins);
     $('#loses').html(loses);
     $('#currentScore').html(currentScore);
-    }
+    };
+
 
     updateResults();
+
 });
+
+
